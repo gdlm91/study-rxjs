44e6770ebe0612cb963561bb1a28a7fbec2d666f
Gerardo Leal <gerard.leal91@gmail.com>
2016-10-28 11:39:34 -0300

    Title Comments Changes

--- src/app.js
+++ src/app.js
@@ -3,15 +3,16 @@ import Rx from 'rxjs/Rx';
 
 console.log('RxJS Boiler Running...');
 
+/** Observables from Events */
+/////////////////////////////
+
 /** Click Event */
-/////////////////
 const btn = document.getElementById("btn");
 const btnStream$ = Rx.Observable.fromEvent(btn, "click");
 
 btnStream$.subscribe(e => console.log("Click Event from btn!"));
 
 /** Key Up Event */
-/////////////////
 const input = document.getElementById("input");
 const output_1 = document.getElementById("output_1");
 const inputStream$ = Rx.Observable.fromEvent(input, "keyup");
@@ -19,7 +20,6 @@ const inputStream$ = Rx.Observable.fromEvent(input, "keyup");
 inputStream$.subscribe(e => output_1.innerHTML = e.target.value);
 
 /** Mouse Move Event */
-//////////////////////
 const output_2 = document.getElementById("output_2");
 const mouseStream$ = Rx.Observable.fromEvent(document, "mousemove");
 
